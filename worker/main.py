c733c9b200d09f1d7ead36efd3243a3d3885e43a
natan-ls <lopesnatan147@gmail.com>
2026-02-03 16:06:22 -0300

    feat(worker): Adicao do servico de email

--- worker/main.py
+++ worker/main.py
@@ -1,5 +1,6 @@
 from confluent_kafka import Consumer
 import json
+import processos
 
 PEDIDO_CRIADO = "pedido-criado"
 ENDERECO = "kafka"
@@ -7,13 +8,15 @@ ENDERECO = "kafka"
 conf = {
     "bootstrap.servers": f"{ENDERECO}:9092",
     "group.id": "pedido-worker",
-    "auto.offset.reset": "earliest"
+    "auto.offset.reset": "earliest",
+    "enable.auto.commit": True,
+    "session.timeout.ms": 10000
 }
 
 consumer = Consumer(conf)
 consumer.subscribe([PEDIDO_CRIADO])
 
-print("Worker aguardando mensagens...")
+print("Worker aguardando mensagens...", flush=True)
 
 while True:
     msg = consumer.poll(1.0)
@@ -21,8 +24,11 @@ while True:
     if msg is None:
         continue
     if msg.error():
-        print(f"Erro: {msg.error()}")
+        print(f"Erro: {msg.error()}", flush=True)
         continue
 
     evento = json.loads(msg.value().decode())
-    print("Evento recebido:", evento)
+    print(f"Evento recebido: {evento} chaves={evento.keys()}", flush=True)
+
+    if evento['tipo_evento'] == 'pedido-criado':
+        processos.handle_pedido_criado(evento)
